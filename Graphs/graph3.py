feaeb62c85d2520a90a3dd2c1ea4f64e2756bce9
Brandon Salazar <salazarbrandon1257@gmail.com>
2020-01-26 15:11:45 -0600

    Update graph3.py

--- Graphs/graph3.py
+++ Graphs/graph3.py
@@ -1,15 +1,11 @@
 import matplotlib.pyplot as plt
-#with open('analysis.txt', 'r') as f:
- #   lines = f.readlines()
-  #  x = [float(line.split()[0]) for line in lines]
-   # y = [float(line.split()[1]) for line in lines]
+#I used SQL to find the average price per rating for the y values
 x=[1, 2, 3, 4, 5]
 y=[35.34, 36.21, 37.19, 33.34, 31.27]
-#plt.plot(x, y)
 
 plt.bar(x, y, width=0.7, bottom=None, align='center')
 plt.xlabel('Rating')
 plt.ylabel('Average Price') 
 plt.title('Average Book Price per rating')
 plt.show()
-#print(25+21+26+20+28)
+
